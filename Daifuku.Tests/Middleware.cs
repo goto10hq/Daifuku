3b0e806c18c1c04ab1dfe25ae642eea8f5afe36a
Joey Frohikey <frohikey@gmail.com>
2017-10-06 16:51:26 +0200

    Add server header

---
+++ Daifuku.Tests/Middleware.cs
@@ -0,0 +1,31 @@
+using Daifuku.Middleware;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Sushi2;
+
+namespace Daifuku.Tests
+{
+    [TestClass]
+    public class Middleware
+    {
+        readonly Mock<HttpContext> httpContextMock = new Mock<HttpContext>();
+        protected Mock<HttpResponse> httpResponse = new Mock<HttpResponse>();
+        readonly Mock<IHeaderDictionary> httpDictionary = new Mock<IHeaderDictionary>();
+
+        [TestInitialize]
+        public void Init()
+        {
+            httpContextMock.SetupGet(a => a.Response).Returns(httpResponse.Object);
+            httpResponse.SetupGet(a => a.Headers).Returns(httpDictionary.Object);
+        }
+
+        [TestMethod]
+        public void ServerHeaderSet()
+        {
+            httpDictionary.SetupSet(a => a[Constants.ServerHeader] = "Goto10").Verifiable();
+            AsyncTools.RunSync(() => new ServerHeader(null, "Goto10").Invoke(httpContextMock.Object));
+            httpDictionary.Verify();
+        }        
+    }
+}
---
+++ Daifuku/Extensions/AppBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Daifuku.Middleware;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+
+namespace Daifuku.Extensions
+{
+    public static class AppBuilderExtensions
+    {
+        public static IApplicationBuilder UseDomainEnforcement(this IApplicationBuilder app, Dictionary<string, string> domainRedirects)
+        {
+            if (app == null)            
+                throw new ArgumentNullException(nameof(app));
+            
+            return app.UseMiddleware<EnforceDomain>(domainRedirects);
+        }
+
+        public static IApplicationBuilder UseServerHeader(this IApplicationBuilder app, string header = null)
+        {
+            if (app == null)
+                throw new ArgumentNullException(nameof(app));
+
+            return app.UseMiddleware<ServerHeader>(header);
+        }
+    }
+}
