744903dd02673b81bbb9274ec84e0fb5aa3e8eb7
Joey Frohikey <frohikey@gmail.com>
2017-10-06 14:50:59 +0200

    Add enfoce domain middleware

---
+++ Daifuku/Middleware/AppBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+
+namespace Daifuku.Middleware
+{
+    public static class AppBuilderExtensions
+    {
+        public static IApplicationBuilder UseDomainEnforcement(this IApplicationBuilder app, Dictionary<string, string> domainRedirects)
+        {
+            if (app == null)            
+                throw new ArgumentNullException(nameof(app));
+            
+            return app.UseMiddleware<EnforceDomainMiddleware>(domainRedirects);
+        }
+    }
+}
---
+++ Daifuku/Middleware/EnforceDomainMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Daifuku.Middleware
+{
+    public class EnforceDomainMiddleware
+    {
+        readonly RequestDelegate _next;
+        Dictionary<string, string> _domainRedirects;
+
+        public EnforceDomainMiddleware(RequestDelegate next, Dictionary<string, string> domainRedirects)
+        {
+            _domainRedirects = domainRedirects;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            HttpRequest req = context.Request;
+
+            var host = req.Host.Host.ToLower();
+            var redirect = _domainRedirects.FirstOrDefault(dr => dr.Key.Equals(host, StringComparison.OrdinalIgnoreCase));
+            
+            if (redirect.Value != null)
+            {                
+                var url = req.Scheme + "://" + redirect.Value + req.Path + req.QueryString;
+                context.Response.Redirect(url, true);                
+            }
+            else            
+            {
+                await _next(context);
+            }
+        }
+    }
+}
