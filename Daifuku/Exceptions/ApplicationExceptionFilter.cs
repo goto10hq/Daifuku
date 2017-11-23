c55e8f8be5089ce50de08ea454fbdfda5df4e11f
Joey Frohikey <frohikey@gmail.com>
2017-11-23 11:30:20 +0100

    Add exceptions

---
+++ Daifuku/Exceptions/ApplicationExceptionFilter.cs
@@ -0,0 +1,42 @@
+﻿using Daifuku.Validations;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+
+namespace Daifuku.Exceptions
+{
+    public sealed class ApplicationExceptionFilter<T> : IExceptionFilter where T : IApplicationException
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var exceptionType = context.Exception.GetType();
+
+            if (context.Exception is IApplicationException appException)
+            {
+                var modelState = context.ModelState;
+
+                if (appException.Messages != null &&
+                    appException.Messages.Any())
+                {
+                    foreach (var m in appException.Messages)
+                        modelState.AddModelError(m.Field ?? "", m.Message);
+                }
+                else if (!string.IsNullOrEmpty(appException.Message))
+                {
+                    modelState.AddModelError("", appException.Message);
+                }
+                else
+                    modelState.AddModelError("", "Internal error.");
+
+                var title = string.Empty;
+
+                var validateModelFilter = context.Filters.FirstOrDefault(f => f.GetType() == typeof(ValidateModelAttribute));
+
+                if (validateModelFilter != null &&
+                    validateModelFilter is ValidateModelAttribute vma)
+                    title = vma.Message;
+
+                context.Result = new ValidationFailedResult(title, context.ModelState);
+            }
+        }
+    }
+}
---
+++ Daifuku/Exceptions/IApplicationException.cs
@@ -0,0 +1,11 @@
+﻿using Daifuku.Validations;
+using System.Collections.Generic;
+
+namespace Daifuku.Exceptions
+{
+    public interface IApplicationException
+    {
+        IEnumerable<ValidationError> Messages { get; }
+        string Message { get; }
+    }
+}
