bce123b50adf8c66919746ab4066244958ce9891
Joey Frohikey <frohikey@gmail.com>
2018-01-31 19:11:04 +0100

    Add HttpRequestExtensions

---
+++ Daifuku/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Daifuku.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static bool IsAjaxRequest(this HttpRequest request, bool fuzzy = true)
+        {
+            if (request == null)
+                throw new ArgumentNullException(nameof(request));
+
+            if (request.Headers != null &&
+                request.Headers["X-Requested-With"].ToString().Equals("XMLHttpRequest", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            if (!fuzzy)
+                return false;
+
+            if (request.ContentType.IndexOf("application/json", StringComparison.OrdinalIgnoreCase) >= 0 &&
+                request.Headers != null &&
+                request.Headers["Accept"].ToString().IndexOf("application/json", StringComparison.OrdinalIgnoreCase) >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
