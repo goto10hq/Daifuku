4071204f5c7e83a2497959bf99616f5ac25d8c86
Joey Frohikey <frohikey@gmail.com>
2018-01-31 18:42:46 +0100

    Add DisplayNameForTagHelper

---
+++ Daifuku/TagHelpers/DisplayNameForTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace Daifuku.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes = DisplayNameForAttribute)]
+    public class DisplayNameForTagHelper : TagHelper
+    {
+        const string DisplayNameForAttribute = "asp-display-name-for";
+
+        [HtmlAttributeName(DisplayNameForAttribute)]
+        public ModelExpression DisplayNameFor { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            var text = DisplayNameFor.ModelExplorer.Metadata.DisplayName ?? DisplayNameFor.ModelExplorer.Metadata.PropertyName;
+
+            output.Content.SetContent(text);
+        }
+    }
+}
