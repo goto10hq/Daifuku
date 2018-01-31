53305f9f3533492246bdc78033e2d9f2358a853b
Joey Frohikey <frohikey@gmail.com>
2018-01-31 19:13:40 +0100

    Add AjaxOnlyAttribute

---
+++ Daifuku/Attributes/AjaxOnlyAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Daifuku.Extensions;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Routing;
+
+namespace Daifuku.Attributes
+{
+    public class AjaxOnlyAttribute : ActionMethodSelectorAttribute
+    {
+        readonly bool _fuzzy = true;
+
+        public AjaxOnlyAttribute()
+        {
+        }
+
+        public AjaxOnlyAttribute(bool fuzzy)
+        {
+            _fuzzy = fuzzy;
+        }
+
+        public override bool IsValidForRequest(RouteContext routeContext, ActionDescriptor action)
+        {
+            return routeContext.HttpContext.Request.IsAjaxRequest(_fuzzy);
+        }
+    }
+}
