83f3c8fe47f61f165d0c6d575eff11e820cb5f2c
Joey Frohikey <frohikey@gmail.com>
2018-02-02 18:12:42 +0100

    Add TempDataTagHelper

---
+++ Daifuku/TagHelpers/TempDataTagHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace Daifuku.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes = "temp-data-key")]
+    public class TempDataTagHelper : TagHelper
+    {
+        [HtmlAttributeName("temp-data-key")]
+        public string Key { get; set; }
+
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        protected ITempDataDictionary TempData => ViewContext.TempData;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            if (TempData[Key] == null)
+            {
+                output.SuppressOutput();
+            }
+            else
+            {
+                output.Content.SetContent(TempData[Key].ToString());
+            }
+        }
+    }
+}
