58d02e6f9e4da952adc9c02243edfced4a90dab0
Joey Frohikey <frohikey@gmail.com>
2018-06-21 11:13:06 +0200

    Basic test for DisplayNameForTagHelper

---
+++ Daifuku.Tests/DisplayNameForTagHelperTests.cs
@@ -0,0 +1,91 @@
+﻿using Daifuku.Extensions;
+using Daifuku.TagHelpers;
+using Daifuku.Tests.Helpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Daifuku.Tests
+{
+    public class DisplayNameForTagHelperTests
+    {
+        static ModelExpression CreateModelExpression(string name)
+        {
+            var modelMetadataProvider = new EmptyModelMetadataProvider();
+
+            return new ModelExpression(name, modelMetadataProvider.GetModelExplorerForType(typeof(Model), new Model()).GetExplorerForProperty(name));
+            //return new ModelExpression(name, modelMetadataProvider.GetModelExplorerForType(typeof(Model), new Model()));
+        }
+
+        public class Model
+        {
+            [DisplayName("Kaos")]
+            public string Moeta { get; set; }
+
+            public int Something { get; set; }
+        }
+
+        //[Fact]
+        // TODO: fix
+        public void DisplayNameForSet()
+        {
+            var m = new Model();
+
+            var th = new DisplayNameForTagHelper
+            {
+                DisplayNameFor = CreateModelExpression("Moeta")
+            };
+
+            var tagHelperContext = new TagHelperContext(
+                            new TagHelperAttributeList(),
+                            new Dictionary<object, object>(),
+                            Guid.NewGuid().ToString("N"));
+
+            var tagHelperOutput = new TagHelperOutput("div",
+                new TagHelperAttributeList(),
+            (result, encoder) =>
+            {
+                var tagHelperContent = new DefaultTagHelperContent();
+                tagHelperContent.SetHtmlContent("");
+                return Task.FromResult<TagHelperContent>(tagHelperContent);
+            });
+            th.Process(tagHelperContext, tagHelperOutput);
+
+            var html = tagHelperOutput.ToHtml();
+            Assert.Equal("<div>Kaos</div>", html);
+        }
+
+        [Fact]
+        public void DisplayNameForNotSet()
+        {
+            var m = new Model();
+
+            var th = new DisplayNameForTagHelper
+            {
+                DisplayNameFor = CreateModelExpression("Something")
+            };
+
+            var tagHelperContext = new TagHelperContext(
+                            new TagHelperAttributeList(),
+                            new Dictionary<object, object>(),
+                            Guid.NewGuid().ToString("N"));
+
+            var tagHelperOutput = new TagHelperOutput("div",
+                new TagHelperAttributeList(),
+            (result, encoder) =>
+            {
+                var tagHelperContent = new DefaultTagHelperContent();
+                tagHelperContent.SetHtmlContent("");
+                return Task.FromResult<TagHelperContent>(tagHelperContent);
+            });
+            th.Process(tagHelperContext, tagHelperOutput);
+
+            var html = tagHelperOutput.ToHtml();
+            Assert.Equal("<div>Something</div>", html);
+        }
+    }
+}
---
+++ Daifuku.Tests/Helpers/EmptyModelMetadataProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+
+namespace Daifuku.Tests.Helpers
+{
+    public class EmptyModelMetadataProvider : DefaultModelMetadataProvider
+    {
+        public EmptyModelMetadataProvider()
+            : base(
+                  new DefaultCompositeMetadataDetailsProvider(new List<IMetadataDetailsProvider>()),
+                  new OptionsAccessor())
+        {
+        }
+
+        class OptionsAccessor : IOptions<MvcOptions>
+        {
+            public MvcOptions Value { get; } = new MvcOptions();
+        }
+    }
+}
