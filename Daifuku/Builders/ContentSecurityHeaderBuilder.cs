8f19a55dd147992af9e09bd51c27e0ac4d55c879
Joey Frohikey <frohikey@gmail.com>
2018-08-24 13:29:46 +0200

    Clean ups

--- Daifuku/Builders/ContentSecurityHeaderBuilder.cs
+++ Daifuku/Builders/ContentSecurityHeaderBuilder.cs
@@ -3,7 +3,7 @@ using System.Text;
 
 namespace Daifuku.Builders
 {
-    class ContentSecurityPolicyBuilder
+    static class ContentSecurityPolicyBuilder
     {
         internal static string Build(ContentSecurityPolicy policy)
         {
@@ -12,83 +12,84 @@ namespace Daifuku.Builders
             if (policy == null)
                 throw new ArgumentNullException(nameof(policy));
 
-            if (policy.DefaultSrc != null && policy.DefaultSrc.Count > 0)
+            if (policy.DefaultSrc?.Count > 0)
             {
                 stringBuilder.Append(Constants.CspDirectives.DefaultSrc);
-                stringBuilder.Append($" {string.Join(" ", policy.DefaultSrc)}; ");
+                stringBuilder.Append(" ").Append(string.Join(" ", policy.DefaultSrc)).Append("; ");
             }
 
-            if (policy.ScriptSrc != null && policy.ScriptSrc.Count > 0)
+            if (policy.ScriptSrc?.Count > 0)
             {
                 stringBuilder.Append(Constants.CspDirectives.ScriptSrc);
-                stringBuilder.Append($" {string.Join(" ", policy.ScriptSrc)}; ");
+                stringBuilder.Append(" ").Append(string.Join(" ", policy.ScriptSrc)).Append("; ");
             }
 
-            if (policy.StyleSrc != null && policy.StyleSrc.Count > 0)
+            if (policy.StyleSrc?.Count > 0)
             {
                 stringBuilder.Append(Constants.CspDirectives.StyleSrc);
-                stringBuilder.Append($" {string.Join(" ", policy.StyleSrc)}; ");
+                stringBuilder.Append(" ").Append(string.Join(" ", policy.StyleSrc)).Append("; ");
             }
 
-            if (policy.ImgSrc != null && policy.ImgSrc.Count > 0)
+            if (policy.ImgSrc?.Count > 0)
             {
                 stringBuilder.Append(Constants.CspDirectives.ImgSrc);
-                stringBuilder.Append($" {string.Join(" ", policy.ImgSrc)}; ");
+                stringBuilder.Append(" ").Append(string.Join(" ", policy.ImgSrc)).Append("; ");
             }
 
-            if (policy.ConnectSrc != null && policy.ConnectSrc.Count > 0)
+            if (policy.ConnectSrc?.Count > 0)
             {
                 stringBuilder.Append(Constants.CspDirectives.ConnectSrc);
-                stringBuilder.Append($" {string.Join(" ", policy.ConnectSrc)}; ");
+                stringBuilder.Append(" ").Append(string.Join(" ", policy.ConnectSrc)).Append("; ");
             }
 
-            if (policy.FontSrc != null && policy.FontSrc.Count > 0)
+            if (policy.FontSrc?.Count > 0)
             {
                 stringBuilder.Append(Constants.CspDirectives.FontSrc);
-                stringBuilder.Append($" {string.Join(" ", policy.FontSrc)}; ");
+                stringBuilder.Append(" ").Append(string.Join(" ", policy.FontSrc)).Append("; ");
             }
 
-            if (policy.ObjectSrc != null && policy.ObjectSrc.Count > 0)
+            if (policy.ObjectSrc?.Count > 0)
             {
                 stringBuilder.Append(Constants.CspDirectives.ObjectSrc);
-                stringBuilder.Append($" {string.Join(" ", policy.ObjectSrc)}; ");
+                stringBuilder.Append(" ").Append(string.Join(" ", policy.ObjectSrc)).Append("; ");
             }
 
-            if (policy.MediaSrc != null && policy.MediaSrc.Count > 0)
+            if (policy.MediaSrc?.Count > 0)
             {
                 stringBuilder.Append(Constants.CspDirectives.MediaSrc);
-                stringBuilder.Append($" {string.Join(" ", policy.MediaSrc)}; ");
+                stringBuilder.Append(" ").Append(string.Join(" ", policy.MediaSrc)).Append("; ");
             }
 
-            if (policy.ChildSrc != null && policy.ChildSrc.Count > 0)
+            if (policy.ChildSrc?.Count > 0)
             {
                 stringBuilder.Append(Constants.CspDirectives.ChildSrc);
-                stringBuilder.Append($" {string.Join(" ", policy.ChildSrc)}; ");
+                stringBuilder.Append(" ").Append(string.Join(" ", policy.ChildSrc)).Append("; ");
             }
 
-            if (policy.FormAction != null && policy.FormAction.Count > 0)
+            if (policy.FormAction?.Count > 0)
             {
                 stringBuilder.Append(Constants.CspDirectives.FormAction);
-                stringBuilder.Append($" {string.Join(" ", policy.FormAction)}; ");
+                stringBuilder.Append(" ").Append(string.Join(" ", policy.FormAction)).Append("; ");
             }
 
-            if (policy.FrameAncestors != null && policy.FrameAncestors.Count > 0)
+            if (policy.FrameAncestors?.Count > 0)
             {
                 stringBuilder.Append(Constants.CspDirectives.FrameAncestors);
-                stringBuilder.Append($" {string.Join(" ", policy.FrameAncestors)}; ");
+                stringBuilder.Append(" ").Append(string.Join(" ", policy.FrameAncestors)).Append("; ");
             }
 
             if (policy.Sandbox != null)
             {
                 stringBuilder.Append(Constants.CspDirectives.Sandbox);
-                stringBuilder.Append($" {policy.Sandbox.Value}; ");
+                stringBuilder.Append(" ").Append(policy.Sandbox.Value).Append("; ");
             }
 
-            if (policy.PluginTypes != null && policy.PluginTypes.Count > 0)
+            if (policy.PluginTypes?.Count > 0)
             {
                 stringBuilder.Append(Constants.CspDirectives.PluginTypes);
-                stringBuilder.Append($" {string.Join(" ", policy.PluginTypes)}; ");
+                stringBuilder.Append(" ").Append(string.Join(" ", policy.PluginTypes)).Append("; ");
             }
+
             return stringBuilder.ToString().TrimEnd();
         }
     }
